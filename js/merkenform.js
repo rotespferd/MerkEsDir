6838efa0f1052d102df8229f8baf11f5d8b77662
Marian Sievers <marian.sievers@gmail.com>
2012-06-01 11:32:17 +0200

    Items are also saved when you hit enter.

--- js/merkenform.js
+++ js/merkenform.js
@@ -1,8 +1,20 @@
 $("#send").click(function() {
-	//Inhalt aus Formular holen
+	saveItem();
+});
+
+$("#input").keydown(function(e) {
+	var code = e.keyCode || e.which;
+	if(code == "13"){
+		saveItem();
+	}
+});
+
+function saveItem() {
 	var input = $("#input").val();
 	input = "<li class='hidden'>" + input + "</li>";
 	$("#merkliste").append(input);
 	$(".hidden").fadeIn(500);
 	$("#input").val("");
-});
+	$("#input").focus();
+	//push input to server
+}
