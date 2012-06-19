c542a1fa3ad82da7b6971312875bde8307beffb1
Marian Sievers <marian.sievers@gmail.com>
2012-06-19 19:38:19 +0200

    The request handler handles messages to /upload. He just print it out.

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,6 +1,7 @@
 var querystring = require("querystring"),
     fs = require("fs"),
-    formidable = require("formidable");
+    formidable = require("formidable"),
+    url = require("url");
 
 function start(response) {
   console.log("Request handler 'start' was called.");
@@ -27,24 +28,16 @@ function start(response) {
 function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
-  var form = new formidable.IncomingForm();
-  console.log("about to parse");
-  form.parse(request, function(error, fields, files) {
-    console.log("parsing done");
+  var url = url.parse(request.url, true);
+  
+  var msg = url.query.merkItem;
+  
+  console.log("Get the merkItem " + msg);
 
-    /* Possible error on Windows systems:
-       tried to rename to an already existing file */
-    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-      if (err) {
-        fs.unlink("/tmp/test.png");
-        fs.rename(files.upload.path, "/tmp/test.png");
-      }
-    });
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("received image:<br/>");
-    response.write("<img src='/show' />");
-    response.end();
-  });
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("received image:<br/>");
+  response.write("<img src='/show' />");
+  response.end();
 }
 
 exports.start = start;
