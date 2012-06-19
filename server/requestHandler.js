cc912b90f4462d401bf73dd4d9501308b385b3f6
Marian Sievers <marian.sievers@gmail.com>
2012-06-19 17:08:24 +0200

    Customize the server code from Manuel Kiesslin

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -47,21 +47,5 @@ function upload(response, request) {
   });
 }
 
-function show(response) {
-  console.log("Request handler 'show' was called.");
-  fs.readFile("/tmp/test.png", "binary", function(error, file) {
-    if(error) {
-      response.writeHead(500, {"Content-Type": "text/plain"});
-      response.write(error + "\n");
-      response.end();
-    } else {
-      response.writeHead(200, {"Content-Type": "image/png"});
-      response.write(file, "binary");
-      response.end();
-    }
-  });
-}
-
 exports.start = start;
 exports.upload = upload;
-exports.show = show;
